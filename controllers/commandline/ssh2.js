e76b6425afeeb7b53b12a541104194a9585d9e49
Pietari Daemonna <pietari.daemonna@gmail.com>
2017-03-28 12:27:05 +0200

    rest from within node

--- controllers/commandline/ssh2.js
+++ controllers/commandline/ssh2.js
@@ -16,116 +16,23 @@
 //     ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
 
 'use strict'
-var Connection = require('ssh2');
+var unirest = require('unirest');
 var express = require('express');
 var router = express.Router();
-var SSHController = require("../event_controller");
-
-var sshc = new SSHController();
-
-sshc.on("started", function() {
-  console.log("[INFO]: SSHController started...")
-})
-
-sshc.on("stopped", function() {
-  console.log("[INFO]: SSHController stopped...")
-})
 
 
-router.get('/:hostname/:username/:password/:command', function (req, res, next) {
+router.get('/', function (req, res, next) {
 
-    var host = req.params.hostname;
-    var port = '22';
-    var username = req.params.username;
-    var password = req.params.password;
-    var passphrase = '1234@#4321';
-    var stdout = '';
-    var stderr = '';
-    var command = req.params.command;
-
-    var conn = new Connection();
-    conn.on('ready', function () {
-        //console.log('Connection :: ready');
-        conn.exec(command, function (err, stream) {
-            if (err) return res.send(err);
-            var stdout = '';
-            var stderr = '';
-            stream.on('exit', function (code, signal) {
-                //console.log('Stream :: exit :: code: ' + code + ', signal: ' + signal);
-            }).on('close', function () {
-                //console.log('Stream :: close');
-                conn.end();
-                res.render('ssh2', { stdout: stdout, stderr: stderr });
-            }).on('data', function (data_out) {
-                //console.log('STDOUT: \n' + data_out);
-                stdout += data_out;
-            }).stderr.on('data', function (data_err) {
-                //console.log('STDERR: \n' + data_err);
-                stderr += data_err;
-            });
+    unirest.post('http://mockbin.com/request')
+        .headers({
+            'Content-Type': 'multipart/form-data'
+        })
+        .field('parameter', 'value') // Form field
+        .attach('file', '/tmp/file') // Attachment
+        .end(function (response) {
+            console.log(response.body);
         });
-    }).connect({
-        host: host,
-        port: port,
-        username: username,
-        password: password,
-        passphrase: passphrase,
-        privateKey: require('fs').readFileSync('./ssh_keys/id_rsa')
-    });
-
-    conn.on('error', function (e) {
-        console.log("Connection failed or timed out");
-        res.render('ssh2', { stdout: stdout, stderr: 'Connection failed or timed out' });
-    });
 });
 
-router.get('/RAW/:hostname/:username/:password/:command', function (req, res, next) {
-
-    var host = req.params.hostname;
-    var port = '22';
-    var username = req.params.username;
-    var password = req.params.password;
-    var passphrase = '1234@#4321';
-    var stdout = '';
-    var stderr = '';
-    var command = req.params.command;
-
-    var conn = new Connection();
-    conn.on('ready', function () {
-        //console.log('Connection :: ready');
-        conn.exec(command, function (err, stream) {
-            if (err) return res.send(err);
-            var stdout = '';
-            var stderr = '';
-            stream.on('exit', function (code, signal) {
-                //console.log('Stream :: exit :: code: ' + code + ', signal: ' + signal);
-            }).on('close', function () {
-                //console.log('Stream :: close');
-                conn.end();
-                res.writeHead(200, {'Content-Type': 'text/plaintext'});  //RENDER TO PLAINTEXT
-                res.write( stdout, stderr);
-                res.end();
-            }).on('data', function (data_out) {
-                //console.log('STDOUT: \n' + data_out);
-                stdout += data_out;
-            }).stderr.on('data', function (data_err) {
-                //console.log('STDERR: \n' + data_err);
-                stderr += data_err;
-            });
-        });
-    }).connect({
-        host: host,
-        port: port,
-        username: username,
-        password: password,
-        passphrase: passphrase,
-        privateKey: require('fs').readFileSync('./ssh_keys/id_rsa')
-    });
-
-    conn.on('error', function (e) {
-        console.log("Connection failed or timed out");
-        res.render('Connection failed or timed out');
-    });
-});
 
 module.exports = router;
